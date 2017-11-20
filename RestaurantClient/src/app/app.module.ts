700ff99ef308aeaf1e7fc29d3c3b2e9a0d0a6594
Cecília Assis <cy_assis@hotmail.com>
2017-11-19 22:26:00 -0200

    [client] restaurant component

--- RestaurantClient/src/app/app.module.ts
+++ RestaurantClient/src/app/app.module.ts
@@ -5,17 +5,24 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { SearchRestaurantComponent } from './restaurants/search-restaurant/search-restaurant.component';
+import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    RestaurantsComponent,
+    SearchRestaurantComponent,
+    RestaurantComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
-      {path: '', component: HomeComponent}
+      {path: '', component: HomeComponent},
+      {path: 'restaurants', component: RestaurantsComponent}
     ])
   ],
   providers: [],
